fc41c58b28a94b3aeaa96949ad36faf931325e03
Arafin Ahmed <arafin@Arafins-Mac-mini.local>
2023-06-21 15:31:11 +0600

    api done

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -30,7 +30,7 @@ async def read_all_summaries() -> List[SummarySchema]:
 
 
 @router.delete("/{id}/", response_model=SummaryResponseSchema)
-async def delete_summary(id: int) -> SummaryResponseSchema:
+async def delete_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
     summay = await crud.get(id)
     if not summay:
         raise HTTPException(status_code=404, detail="Summary not found")
@@ -41,7 +41,7 @@ async def delete_summary(id: int) -> SummaryResponseSchema:
 
 
 @router.put("/{id}/", response_model=SummarySchema)
-async def update_summary(id: int, payload: SummaryUpdatePayloadSchema) -> SummarySchema:
+async def update_summary(payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.put(id, payload)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
